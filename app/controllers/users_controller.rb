751f12db03ca7caf25879368dafe7a33117b363e
Gerald Bivens <geraldbivens@gmail.com>
2021-03-16 11:40:12 -0600

    Add users controller actions and strong params, and create a new user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+        render json: @users
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    def create
+        @user = User.create(user_params)
+        render json: @user
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit([:name, :username, :email])
+    end
+
 end
