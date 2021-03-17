26ec0a074ff5e5488c290674f7af369a6fc914a5
Gerald Bivens <geraldbivens@gmail.com>
2021-03-17 01:40:31 -0600

    Add secure password and update validations in the user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
-    validates :name, :username, :email, presence: true
-    validates :username, length: { minimum: 6, maximum: 12 }
+    has_secure_password
+
+    # validates :name, :username, :email, :password, presence: true
+    validates :name, :username, :email, :password, presence: {message: "%{attribute} cannot be blank."}
+    validates :username, length: {minimum: 6, maximum: 12, message: "%{attribute} must be between 6 and 12 characters long."}
+    validates :username, uniqueness: {message: "%{attribute}s must be unique, and %{value} has already been taken."}
 end
