e57644a63f4583be34ce4bef9ee8ff91d071cd18
Gerald Bivens <geraldbivens@gmail.com>
2021-03-17 01:28:59 -0600

    Add conditional logic and error messages to users controller create and update actions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,19 +15,37 @@ class UsersController < ApplicationController
     # end
 
     # Basic example of error handling
+    # def create
+    #     @user = User.new(user_params)
+    #     if @user.save
+    #         render json: @user
+    #     else
+    #         render status: 418
+    #     end
+    # end
+
     def create
         @user = User.new(user_params)
         if @user.save
             render json: @user
         else
-            render status: 418
+            render json: @user.errors.messages
         end
     end
 
+    # def update
+    #     @user = User.find(params[:id])
+    #     @user.update(user_params)
+    #     render json: @user
+    # end
+
     def update
         @user = User.find(params[:id])
-        @user.update(user_params)
-        render json: @user
+        if @user.update
+            render json: @user
+        else
+            render json: @user.errors.messages
+        end
     end
 
     def destroy
